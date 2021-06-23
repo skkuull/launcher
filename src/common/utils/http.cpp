661889e2ab4249bb7778f4b7e16b965868d4f1de
Maurice Heumann <maurice.heumann@wibu.com>
2021-06-23 10:20:04 +0200

    Fix download

--- src/common/utils/http.cpp
+++ src/common/utils/http.cpp
@@ -8,23 +8,38 @@ namespace utils::http
 {
 	namespace
 	{
-		struct write_helper
+		struct progress_helper
 		{
-			const std::function<void(size_t)>* callback;
-			std::string* data;
+			const std::function<void(size_t)>* callback{};
+			std::exception_ptr exception{};
 		};
+
+		int progress_callback(void *clientp, const curl_off_t /*dltotal*/, const curl_off_t dlnow, const curl_off_t /*ultotal*/, const curl_off_t /*ulnow*/)
+		{
+			auto* helper = static_cast<progress_helper*>(clientp);
+
+			try
+			{
+				if (*helper->callback)
+				{
+					(*helper->callback)(dlnow);
+				}
+			}
+			catch(...)
+			{
+				helper->exception = std::current_exception();
+				return -1;
+			}
+
+			return 0;
+		}
 	
 		size_t write_callback(void* contents, const size_t size, const size_t nmemb, void* userp)
 		{
-			auto* helper = static_cast<write_helper*>(userp);
+			auto* buffer = static_cast<std::string*>(userp);
 
 			const auto total_size = size * nmemb;
-			helper->data->append(static_cast<char*>(contents), total_size);
-			if (*helper->callback)
-			{
-				OutputDebugStringA(std::to_string(total_size).data());
-				(*helper->callback)(helper->data->size());
-			}
+			buffer->append(static_cast<char*>(contents), total_size);
 			return total_size;
 		}
 	}
@@ -43,7 +58,6 @@ namespace utils::http
 			curl_slist_free_all(header_list);
 			curl_easy_cleanup(curl);
 		});
-
 		
 		for(const auto& header : headers)
 		{
@@ -52,18 +66,25 @@ namespace utils::http
 		}
 
 		std::string buffer{};
-		write_helper helper{};
-		helper.data = &buffer;
+		progress_helper helper{};
 		helper.callback = &callback;
 		
 		curl_easy_setopt(curl, CURLOPT_HTTPHEADER, header_list);
 		curl_easy_setopt(curl, CURLOPT_URL, url.data());
 		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
-		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &helper);
-		curl_easy_setopt(curl, CURLOPT_TIMEOUT, 1);
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer);
+		curl_easy_setopt(curl, CURLOPT_XFERINFOFUNCTION, progress_callback);
+		curl_easy_setopt(curl, CURLOPT_XFERINFODATA, &helper);
+		curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 0);
+
 		if (curl_easy_perform(curl) == CURLE_OK)
 		{
 			return {std::move(buffer)};
+		}
+
+		if(helper.exception)
+		{
+			std::rethrow_exception(helper.exception);
 		}
 
 		return {};

40601c4d825972da792e4e4d8fa6420c869fae2d
Maurice Heumann <maurice.heumann@wibu.com>
2021-06-26 10:56:44 +0200

    More output

--- get-cef.ps1
+++ get-cef.ps1
@@ -1,23 +1,35 @@
 $cefVersion = 'cef_binary_91.1.21+g9dd45fe+chromium-91.0.4472.114_windows64'
 #-------------------------------------------------
 
+Write-Output 'Deleting old CEF binaries...'
+
+$destinationPart = 'cef.tar'
+$destination = "$destinationPart.bz2"
+
 $cefPath = 'deps/cef'
+Remove-Item $destinationPart -ErrorAction Ignore
+Remove-Item $destination -ErrorAction Ignore
 Remove-Item -LiteralPath $cefPath -Force -Recurse -ErrorAction Ignore
+
+Write-Output 'Downloading CEF...'
 
 Add-Type -AssemblyName System.Web
 $cefURLVersion = [System.Web.HttpUtility]::UrlEncode($cefVersion) 
 
 $source = "https://cef-builds.spotifycdn.com/$cefURLVersion.tar.bz2"
-$destinationPart = 'cef.tar'
-$destination = "$destinationPart.bz2"
 (New-Object Net.WebClient).DownloadFile($source, $destination)
 
 $sz = '"C:\Program Files\7-Zip\7z.exe"'
 
+Write-Output 'Unpacking CEF...'
 cmd /c "$sz x $destination -aoa"
 cmd /c "$sz x $destinationPart -aoa"
 
 Move-Item -Path $cefVersion -Destination $cefPath
 
+Write-Output 'Doing cleanup...'
+
 Remove-Item $destinationPart
 Remove-Item $destination
+
+Write-Output 'Done!'

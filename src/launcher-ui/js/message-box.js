f008cd244242a0675b6b4e4e7535f176096e5ca5
momo5502 <mauriceheumann@gmail.com>
2021-07-06 19:13:00 +0200

    Add messageboxes

---
+++ src/launcher-ui/js/message-box.js
@@ -0,0 +1,47 @@
+function constructMessageBox(title, message, buttons) {
+    title = title || "Error";
+    nessage = message || "";
+    buttons = buttons || ["Ok"];
+
+    return new Promise((resolve, reject) => {
+        try{
+            const messageBox = document.querySelector("#message-box");
+
+            const resolveInternal = function() {
+                messageBox.classList.remove("visible");
+                return resolve.apply(this, arguments);
+            };
+
+            const titleElement = messageBox.querySelector(".mb-title");
+            titleElement.textContent = title;
+
+            const contentElement = messageBox.querySelector(".mb-content");
+            contentElement.textContent = message;
+
+            const buttonElement = messageBox.querySelector(".mb-buttons");
+            buttonElement.textContent = "";
+
+            messageBox.classList.add("visible");
+
+            buttons.forEach((value, index) => {
+
+                var button = document.createElement("button");
+                button.textContent = value;
+                button.onclick = resolveInternal.bind(this, index);
+                buttonElement.appendChild(button);
+            });
+        }
+        catch(e) {
+            reject(e);
+        }
+    });
+}
+
+window.showMessageBox = function() {
+    window.lastBox = window.lastBox || Promise.resolve();
+
+    const newBox = window.lastBox.then(() => constructMessageBox.apply(this, arguments));
+    window.lastBox = newBox;
+
+    return newBox;
+};

a07af1caa6732eb684dba1042a58af80c99fcd41
momo5502 <mauriceheumann@gmail.com>
2021-07-11 14:35:18 +0200

    Support launching AW

--- src/launcher/main.cpp
+++ src/launcher/main.cpp
@@ -10,124 +10,6 @@
 
 namespace
 {
-	bool is_subprocess()
-	{
-		return strstr(GetCommandLineA(), "--xlabs-subprocess");
-	}
-
-	int run_subprocess(const utils::nt::library& process, const std::string& path)
-	{
-		const cef::cef_ui cef_ui{process, path};
-		return cef_ui.run_process();
-	}
-
-	void add_commands(cef::cef_ui& cef_ui)
-	{
-		cef_ui.add_command("browse-folder", [](const auto&, rapidjson::Document& response)
-		{
-			response.SetNull();
-
-			std::string folder{};
-			if (utils::com::select_folder(folder))
-			{
-				response.SetString(folder, response.GetAllocator());
-			}
-		});
-
-		cef_ui.add_command("close", [&cef_ui](const auto&, auto&)
-		{
-			cef_ui.close_browser();
-		});
-
-		cef_ui.add_command("minimize", [&cef_ui](const auto&, auto&)
-		{
-			ShowWindow(cef_ui.get_window(), SW_MINIMIZE);
-		});
-
-		cef_ui.add_command("show", [&cef_ui](const auto&, auto&)
-		{
-			auto* const window = cef_ui.get_window();
-			ShowWindow(window, SW_SHOWDEFAULT);
-			SetForegroundWindow(window);
-
-			PostMessageA(window, WM_DELAYEDDPICHANGE, 0, 0);
-		});
-
-		cef_ui.add_command("get-property", [](const rapidjson::Value& value, rapidjson::Document& response)
-		{
-			response.SetNull();
-
-			if (!value.IsString())
-			{
-				return;
-			}
-
-			const std::string key{value.GetString(), value.GetStringLength()};
-			const auto property = utils::properties::load(key);
-			if(!property)
-			{
-				return;
-			}
-			
-			response.SetString(*property, response.GetAllocator());
-		});
-
-		cef_ui.add_command("set-property", [](const rapidjson::Value& value, auto&)
-		{
-			if (!value.IsObject())
-			{
-				return;
-			}
-
-			const auto _ = utils::properties::lock();
-
-			for(auto i = value.MemberBegin(); i != value.MemberEnd(); ++i)
-			{
-				if(!i->value.IsString())
-				{
-					continue;
-				}
-				
-				const std::string key{i->name.GetString(), i->name.GetStringLength()};
-				const std::string val{i->value.GetString(), i->value.GetStringLength()};
-
-				utils::properties::store(key, val);
-			}
-		});
-
-		cef_ui.add_command("get-channel", [&cef_ui](auto&, rapidjson::Document& response)
-		{
-			const std::string channel = updater::is_main_channel() ? "main" : "dev";
-			response.SetString(channel, response.GetAllocator());
-		});
-
-		cef_ui.add_command("switch-channel", [&cef_ui](const rapidjson::Value& value, auto&)
-		{
-			if (!value.IsString())
-			{
-				return;
-			}
-			
-			const std::string channel{value.GetString(), value.GetStringLength()};
-			const auto* const command_line = channel == "main" ? "--xlabs-channel-main" : "--xlabs-channel-develop";
-
-			utils::at_exit([command_line]()
-			{
-				utils::nt::relaunch_self(command_line);
-			});
-
-			cef_ui.close_browser();
-		});
-	}
-
-	void show_window(const utils::nt::library& process, const std::string& path)
-	{
-		cef::cef_ui cef_ui{process, path};
-		add_commands(cef_ui);
-		cef_ui.create(path + "data/launcher-ui", "main.html");
-		cef_ui.work();
-	}
-
 	std::string get_appdata_path()
 	{
 		PWSTR path;
@@ -170,6 +52,160 @@ namespace
 		{
 			throw std::runtime_error{"X Labs launcher is already running"};
 		}
+	}
+
+	bool is_subprocess()
+	{
+		return strstr(GetCommandLineA(), "--xlabs-subprocess");
+	}
+
+	int run_subprocess(const utils::nt::library& process, const std::string& path)
+	{
+		const cef::cef_ui cef_ui{process, path};
+		return cef_ui.run_process();
+	}
+
+	void add_commands(cef::cef_ui& cef_ui)
+	{
+		cef_ui.add_command("launch-aw", [&cef_ui](const rapidjson::Value& value, auto&)
+		{
+			if (!value.IsString())
+			{
+				return;
+			}
+
+			std::string arg{value.GetString(), value.GetStringLength()};
+
+			static const std::unordered_map<std::string, std::string> arg_mapping = {
+				{"aw-sp", "-singleplayer"},
+				{"aw-mp", "-multiplayer"},
+				{"aw-zm", "-zombies"},
+				{"aw-survival", "-survival"},
+			};
+
+			const auto mapped_arg = arg_mapping.find(arg);
+			if (mapped_arg == arg_mapping.end())
+			{
+				return;
+			}
+
+			const auto aw_install = utils::properties::load("aw-install");
+			if (!aw_install)
+			{
+				return;
+			}
+
+			SetEnvironmentVariableA("XLABS_AW_INSTALL", aw_install->data());
+
+			const auto s1x_exe = get_appdata_path() + "data/s1x/s1x.exe";
+			utils::nt::launch_process(s1x_exe, mapped_arg->second);
+
+			cef_ui.close_browser();
+		});
+
+		cef_ui.add_command("browse-folder", [](const auto&, rapidjson::Document& response)
+		{
+			response.SetNull();
+
+			std::string folder{};
+			if (utils::com::select_folder(folder))
+			{
+				response.SetString(folder, response.GetAllocator());
+			}
+		});
+
+		cef_ui.add_command("close", [&cef_ui](const auto&, auto&)
+		{
+			cef_ui.close_browser();
+		});
+
+		cef_ui.add_command("minimize", [&cef_ui](const auto&, auto&)
+		{
+			ShowWindow(cef_ui.get_window(), SW_MINIMIZE);
+		});
+
+		cef_ui.add_command("show", [&cef_ui](const auto&, auto&)
+		{
+			auto* const window = cef_ui.get_window();
+			ShowWindow(window, SW_SHOWDEFAULT);
+			SetForegroundWindow(window);
+
+			PostMessageA(window, WM_DELAYEDDPICHANGE, 0, 0);
+		});
+
+		cef_ui.add_command("get-property", [](const rapidjson::Value& value, rapidjson::Document& response)
+		{
+			response.SetNull();
+
+			if (!value.IsString())
+			{
+				return;
+			}
+
+			const std::string key{value.GetString(), value.GetStringLength()};
+			const auto property = utils::properties::load(key);
+			if (!property)
+			{
+				return;
+			}
+
+			response.SetString(*property, response.GetAllocator());
+		});
+
+		cef_ui.add_command("set-property", [](const rapidjson::Value& value, auto&)
+		{
+			if (!value.IsObject())
+			{
+				return;
+			}
+
+			const auto _ = utils::properties::lock();
+
+			for (auto i = value.MemberBegin(); i != value.MemberEnd(); ++i)
+			{
+				if (!i->value.IsString())
+				{
+					continue;
+				}
+
+				const std::string key{i->name.GetString(), i->name.GetStringLength()};
+				const std::string val{i->value.GetString(), i->value.GetStringLength()};
+
+				utils::properties::store(key, val);
+			}
+		});
+
+		cef_ui.add_command("get-channel", [&cef_ui](auto&, rapidjson::Document& response)
+		{
+			const std::string channel = updater::is_main_channel() ? "main" : "dev";
+			response.SetString(channel, response.GetAllocator());
+		});
+
+		cef_ui.add_command("switch-channel", [&cef_ui](const rapidjson::Value& value, auto&)
+		{
+			if (!value.IsString())
+			{
+				return;
+			}
+
+			const std::string channel{value.GetString(), value.GetStringLength()};
+			const auto* const command_line = channel == "main" ? "--xlabs-channel-main" : "--xlabs-channel-develop";
+
+			utils::at_exit([command_line]()
+			{
+				utils::nt::relaunch_self(command_line);
+			});
+
+			cef_ui.close_browser();
+		});
+	}
+
+	void show_window(const utils::nt::library& process, const std::string& path)
+	{
+		cef::cef_ui cef_ui{process, path};
+		add_commands(cef_ui);
+		cef_ui.create(path + "data/launcher-ui", "main.html");
+		cef_ui.work();
 	}
 }
 
